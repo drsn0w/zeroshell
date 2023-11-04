32a96382db0053e8861410fe42963ebd4ea87ee3
drsn0w <liam.crabbe.3@gmail.com>
2023-11-03 19:12:51 -0500

    shell.c: bugfixes and execve

--- src/shell.c
+++ src/shell.c
@@ -22,7 +22,7 @@ void zeroshell_loop(char** env) {
                 printf("%s\n", env[pos]);
                 pos++;
             }
-        } else if(input[0] == ((void*)0)) {
+        } else if(input[0] == '\0' || input[0] == ' ') {
             // If they enter nothing, do nothing
         }
         else {
@@ -44,15 +44,10 @@ void zeroshell_launch(char* input, char** env) {
     char* concatenated;
     while (path_tok[pos] != NULL) {
         // concatenate path and name of program
-        concatenated = malloc(strlen(path_tok[pos]) + strlen(argv[0]) + 1);
-        strcpy(concatenated, path_tok[pos]);
-        strcat(concatenated, "/");
-        strcat(concatenated, argv[0]);
-        int access_result = access(concatenated, X_OK);
-        if(access_result == 0) {
-            printf("%s is valid\n", concatenated);
-            break;
-        }
+        size_t concatenated_size = strlen(path_tok[pos]) + strlen(argv[0]) + 1;
+        concatenated = malloc(concatenated_size);
+        snprintf(concatenated, concatenated_size+1, "%s/%s", path_tok[pos], argv[0]);
+        if(!access(concatenated, X_OK)) break;
         pos++;
     }
     free(path);
@@ -65,8 +60,7 @@ void zeroshell_launch(char* input, char** env) {
             exit(EXIT_FAILURE);
         case 0:
             // Child
-            printf("executing %s\n", argv[0]);
-            execvp(concatenated, argv);
+            execve(concatenated, argv, env);
             // don't expect to return
             perror("zeroshell");
             exit(EXIT_FAILURE);
